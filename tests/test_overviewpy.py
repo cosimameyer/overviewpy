4dea9eb2b7ae60276ed87ca382e3a4a90eeb12a9
cosimameyer <cosima.meyer@gmail.com>
2023-07-19 20:25:37 +0200

    test: add pseudo test for overview_na

--- tests/test_overviewpy.py
+++ tests/test_overviewpy.py
@@ -1,5 +1,7 @@
+import matplotlib
 import pandas as pd
-from overviewpy.overviewpy import overview_tab
+import numpy as np
+from overviewpy.overviewpy import overview_tab, overview_na
 
 def test_overview_tab():
     """Tests shape of output of overview_tab"""
@@ -20,3 +22,19 @@ def test_overview_tab():
     df_actual = overview_tab(df, 'id', 'time')
 
     assert df_actual.shape == df_expected.shape, "DataFrame does not match the expected shape"
+    
+
+#def test_overview_na():
+#    """Test plotting of missing values."""
+#    data_na = {
+#        'id': ['RWA', 'RWA', 'RWA', np.nan, 'GAB', 'GAB', 'FRA', 'FRA', 'BEL', 'BEL', 'ARG', np.nan,  np.nan],
+#        'year': [2022, 2001, 2000, 2023, 2021, 2023, 2020, 2019,  np.nan, 2015, 2014, 2013, 2002]
+#    }
+
+#    df_na = pd.DataFrame(data_na)
+
+#    fig = overview_na(df_na)
+#    assert isinstance(fig, matplotlib.container.BarContainer), \
+#           "Wrong plot type"
+#    assert len(fig.datavalues) == len(df_na.columns), \
+#           "Incorrect number of bars plotted"
