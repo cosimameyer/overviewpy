dd4ce65200a1f157e8696177c2c22da064aa30fc
cosimameyer <cosima.meyer@gmail.com>
2023-07-19 08:31:32 +0200

    feat: Add more features to make overview_tab work

--- overviewpy/src/overviewpy/overviewpy.py
+++ overviewpy/src/overviewpy/overviewpy.py
@@ -3,15 +3,12 @@ import pandas as pd
 import numpy as np
 
 def _combine_years(years):
-    if years.isnull().all() or len(years) == 1:
-        return str(int(years.iloc[0].year))
-    else:
-        first_year = years.iloc[0].year
-        last_year = years.iloc[-1].year
-        if last_year - first_year == len(years) - 1:
-            return str(first_year) + '-' + str(last_year)
-        else:
-            return str(int(years.iloc[0].year))
+  min_year = min(years.dt.year)
+  max_year = max(years.dt.year)
+  if min_year == max_year:
+    return str(min_year)
+  else:
+    return f"{str(min_year)}-{str(max_year)}"
 
 def overview_tab(df, id, time):
 
@@ -20,21 +17,17 @@ def overview_tab(df, id, time):
         print("There is a missing value in your id variable. The missing value is automatically deleted.")
         
     df_no_dup = df2.filter(items=[id, time]).drop_duplicates()
+    
     if len(df_no_dup) == len(df2):
       df['time'] = pd.to_datetime(df['time'], format='%Y')  # Convert 'time' column to datetime with year format
 
       # Sort the DataFrame by 'id' and 'time' columns
       df.sort_values(['id', 'time'], inplace=True)
 
-      # Calculate the time differences in years within each group
-      df['consecutive_years'] = df.groupby('id')['time'].diff().dt.days / 365
+      overview = df.groupby('id')['time'].apply(_combine_years)
 
-      # Create a function to combine consecutive years as 'YYYY-YYYY'
+      return pd.DataFrame(overview).reset_index()
 
-      # Convert consecutive years to the desired format
-      df['years'] = df.groupby('id')['time'].apply(_combine_years)
-
-      return df
     else:
         print("There are some duplicates. Make sure to aggregate first.")
             
