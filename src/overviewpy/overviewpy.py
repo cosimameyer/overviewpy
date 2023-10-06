7470dca8d3b0e5788fbf21665e051aab13dfaf64
cosimameyer <cosima.meyer@gmail.com>
2023-10-06 21:17:13 +0200

    bug: fixing bugs in overview_tab

--- src/overviewpy/overviewpy.py
+++ src/overviewpy/overviewpy.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.figure
 import pandas as pd
 
-def overview_tab(df: pd.DataFrame, id: str, time: str) -> pd.DataFrame:
+def overview_tab(df: pd.DataFrame, id: str, time: int) -> pd.DataFrame:
     """Generates a tabular overview of the sample (and returns a data frame). 
     The general sample plots a two-column table that provides information on an 
     id in the left column and a the time frame on the right column.
@@ -10,7 +10,11 @@ def overview_tab(df: pd.DataFrame, id: str, time: str) -> pd.DataFrame:
     Args:
         df (pd.DataFrame): Input data frame
         id (str): Identifies the id column (for instance, country)
-        time (str): Identifies the time column (for instance, years)
+        time (int): Identifies the time column (for instance, years). 
+                    This argument can currently handle simple digits 
+                    (YYYY or YY, for instance).
+                    More complex dates (YYYY-MM-DD, for instance) is 
+                    planned as a future feature.
 
     Returns:
         pd.DataFrame: Returns a reduced data frame that shows a cohesive
@@ -23,50 +27,49 @@ def overview_tab(df: pd.DataFrame, id: str, time: str) -> pd.DataFrame:
         
     df_no_dup = df2.filter(items=[id, time]).drop_duplicates()
     
-    if len(df_no_dup) == len(df2):
-        df_sorted = df_no_dup.sort_values([id, time])
-      
-        # Group the DataFrame by the ID column
-        grouped = df_sorted.groupby(id)
+    if len(df_no_dup) != len(df2):
+        print("There are some duplicates. We aggregate the data before proceeding.")
         
-        # Initialize the combined column
-        df['time_frame'] = df_no_dup[time].astype(str)
-        
-        # Check if numbers within each group are consecutive and combine them
-        for group_name, group_df in grouped:
-            numbers = group_df[time].tolist()
+    df_sorted = df_no_dup.sort_values([id, time])
+    
+    # Group the DataFrame by the ID column
+    grouped = df_sorted.groupby(id)
+    
+    # Initialize the combined column
+    df['time_frame'] = df_no_dup[time].astype(str)
+    
+    # Check if numbers within each group are consecutive and combine them
+    for _, group_df in grouped:
+        numbers = group_df[time].tolist()
 
-            combined_str = ""
+        combined_str = ""
 
-            if len(numbers) > 1:
-                consecutive_ranges = []
-                current_range = [numbers[0]]
+        if len(numbers) > 1:
+            consecutive_ranges = []
+            current_range = [numbers[0]]
 
-                for i in range(1, len(numbers)):
-                    if numbers[i] == numbers[i-1] + 1:
-                        current_range.append(numbers[i])
-                    else:
-                        if len(current_range) > 1:
-                            consecutive_ranges.append(f'{current_range[0]}-{current_range[-1]}')
-                        else:
-                            consecutive_ranges.append(str(current_range[0]))
-                        current_range = [numbers[i]]
-
-                if len(current_range) > 1:
-                    consecutive_ranges.append(f'{current_range[0]}-{current_range[-1]}')
+            for i in range(1, len(numbers)):
+                if numbers[i] == numbers[i-1] + 1:
+                    current_range.append(numbers[i])
                 else:
-                    consecutive_ranges.append(str(current_range[0]))
+                    if len(current_range) > 1:
+                        consecutive_ranges.append(f'{current_range[0]}-{current_range[-1]}')
+                    else:
+                        consecutive_ranges.append(str(current_range[0]))
+                    current_range = [numbers[i]]
 
-                combined_str = ', '.join(consecutive_ranges)
+            if len(current_range) > 1:
+                consecutive_ranges.append(f'{current_range[0]}-{current_range[-1]}')
             else:
-                combined_str = str(numbers[0])
+                consecutive_ranges.append(str(current_range[0]))
 
-            df_no_dup.loc[group_df.index, 'time_frame'] = combined_str
-            
-        return df_no_dup[[id, 'time_frame']].sort_values([id]).drop_duplicates()
+            combined_str = ', '.join(consecutive_ranges)
+        else:
+            combined_str = str(numbers[0])
 
-    else:
-        print("There are some duplicates. Make sure to aggregate first.")
+        df_no_dup.loc[group_df.index, 'time_frame'] = combined_str
+        
+    return df_no_dup[[id, 'time_frame']].sort_values([id]).drop_duplicates()
             
 def overview_na(df: pd.DataFrame) -> matplotlib.figure.Figure:
     """Plots an overview of missing values by variable.
